4309459fa31300297efa6e74b1124049212a2975
IzzatilloSadirov <sadirovizzatillo3@gmail.com>
2026-02-23 16:46:09 +0500

    add: some changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
     env: {
         NODE_ENV: 'production',
         PORT: 3000,
-        DATABASE_URL: 'postgres://izzatillo:12345@localhost:5432/izzatillo',
+        DATABASE_URL: 'postgres://izzatillo:a-z123456789@localhost:5432/distributor',
         JWT_SECRET: 'tillo',
         TELEGRAM_BOT_USERNAME: 'teztarqatbot',
         TELEGRAM_BOT_TOKEN: '8216268214:AAGyX8htytz8wyQk3_y7ZgDIBjJlv0nGaXk'
