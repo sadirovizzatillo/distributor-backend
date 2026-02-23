7ce888770df266cad26b01935446c63835564072
IzzatilloSadirov <sadirovizzatillo3@gmail.com>
2026-02-23 16:29:02 +0500

    add: some changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,8 +7,12 @@ module.exports = {
     watch: false,
     max_memory_restart: '1G',
     env: {
-      NODE_ENV: 'production',
-      PORT: 3000
+        NODE_ENV: 'production',
+        PORT: 3000,
+        DATABASE_URL: 'postgres://izzatillo:12345@localhost:5432/izzatillo',
+        JWT_SECRET: 'tillo',
+        TELEGRAM_BOT_USERNAME: 'teztarqatbot',
+        TELEGRAM_BOT_TOKEN: '8216268214:AAGyX8htytz8wyQk3_y7ZgDIBjJlv0nGaXk'
     }
   }]
 };
